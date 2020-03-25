e5bc6878d902ac7707b7a33df864f0638d83367d
Alexey Burets <leshik_k_k@mail.ru>
2020-03-25 17:54:03 +0300

    test

---
+++ BSTree/BSTree.js
@@ -0,0 +1,79 @@
+﻿ export class Node {
+    constructor(value, pos) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+        if (!this.pos){
+            this.pos = pos;}
+        else {
+            this.pos = "";
+        }
+    }
+
+    addNode(node, value, pos){
+        if ( !node ) {
+            return new Node(value, pos);
+        }
+        if (node.value > value ){
+            pos += " left ";
+            node.left = this.addNode( node.left,  value, pos);
+        } else if (node.value < value) {
+            pos += " right ";
+            node.right = this.addNode(node.right, value, pos);
+        } else if (node.value === value){
+            node.value = value;
+        }
+        return node;
+    }
+    getNode(value){
+
+        let currentNode = this.node;
+        while (currentNode != null){
+            if (currentNode.value > value) currentNode = currentNode.left;
+            else if (currentNode.value < value) currentNode = currentNode.right;
+            else if (value === currentNode.value) return currentNode;
+        }
+
+        return null;
+    }
+    getMin(node){
+        if (!node.left) return node.value;
+        return this.getMin(node.left);
+    }
+    getMax(node){
+        if (!node.right) return node.value;
+        return this.getMax(node.right);
+    }
+    showPosNode(value){
+        let currentNode = this.node;
+        while (currentNode != null){
+            if (currentNode.value > value) currentNode = currentNode.left;
+            else if (currentNode.value < value) currentNode = currentNode.right;
+            else if (value === currentNode.value) return console.log(`Path this node  is ${currentNode.pos}`);
+        }
+
+        return null;
+    }
+
+
+}
+  export class BinaryTree extends  Node{
+    constructor(value) {
+        super(value);
+        this.node = null;
+    }
+    add(value){
+        this.node = super.addNode(this.node, value);
+    }
+    getMaxNode(){
+        return super.getMax(this.node)
+    }
+    getMinNode(){
+        return super.getMin(this.node)
+    }
+    show(value) {
+        super.showPosNode(value)
+
+    }
+}
+
--- test.js
+++ test.js
@@ -1,5 +1,22 @@
-(function () {
-alert("hi");
-alert("hello");
-    console.log(1);
-})();
+ //import {BinaryTree} from "./BSTree/BSTree"
+
+
+    let test = new BinaryTree();
+    test.add(2);
+    test.add(1);
+    test.add(3);
+    test.add(8);
+    test.add(4);
+    test.add(12);
+    console.log(test);
+
+    let min = test.getMinNode();
+    console.log(min);
+    let max = test.getMaxNode();
+    console.log(max);
+    let get = test.getNode(12);
+    console.log(get);
+
+    test.show(12);
+
+  //  console.log(test);
